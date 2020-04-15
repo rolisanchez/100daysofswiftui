c488a28415f21e899f2e0710cdd00fcfd7aefb7d
rolisanchez <roli.s91@gmail.com>
2020-04-15 16:51:05 +0800

    Day16

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Victor Rolando Sanchez Jara on 4/15/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
