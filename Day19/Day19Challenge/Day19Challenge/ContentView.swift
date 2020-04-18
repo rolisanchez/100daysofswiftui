04abb22cd1d25d3eb9542323af1b24ab08a9ab7d
rolisanchez <roli.s91@gmail.com>
2020-04-18 17:12:26 +0800

    Use Apple Measurements on Day19

--- Day19/Day19Challenge/Day19Challenge/ContentView.swift
+++ Day19/Day19Challenge/Day19Challenge/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: Properties
-    var temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    var temperatureUnitStrings = ["Celsius", "Fahrenheit", "Kelvin"]
+    var temperatureUnits: [UnitTemperature] = [.celsius, .fahrenheit, .kelvin]
     @State private var selectedInput = 1
     @State private var inputValue = ""
     @State private var selectedOutput = 1
@@ -20,39 +21,13 @@ struct ContentView: View {
     let FAHRENHEIT = 1
     let KELVIN = 2
     
-    var output: Double {
+    var input: Measurement<UnitTemperature> {
         let doubleInput = Double(inputValue) ?? 0
-        
-        switch selectedInput {
-            case CELSIUS:
-                switch selectedOutput {
-                    case FAHRENHEIT:
-                        return convertCelsiusToFahr(doubleInput)
-                    case KELVIN:
-                        return convertCelsiusToKelvin(doubleInput)
-                    default:
-                        return doubleInput
-                }
-            case FAHRENHEIT:
-                switch selectedOutput {
-                    case CELSIUS:
-                        return convertFahrToCelsius(doubleInput)
-                    case KELVIN:
-                        return convertFahrToKelvin(doubleInput)
-                    default:
-                        return doubleInput
-            }
-            default:
-                switch selectedOutput {
-                    case CELSIUS:
-                        return convertKelvinToCelsius(doubleInput)
-                    case FAHRENHEIT:
-                        return convertKelvinToFahr(doubleInput)
-                    default:
-                        return doubleInput
-            }
-            
-        }
+        return Measurement(value: doubleInput, unit: temperatureUnits[selectedInput])
+    }
+
+    var output: Measurement<UnitTemperature> {
+        return input.converted(to: temperatureUnits[selectedOutput])
     }
     
     // MARK: Body
@@ -61,26 +36,26 @@ struct ContentView: View {
             Form {
                 Section(header: Text("Select input unit")) {
                     Picker("Input Unit", selection: $selectedInput) {
-                        ForEach(0 ..< temperatureUnits.count) {
-                            Text("\(self.temperatureUnits[$0])")
+                        ForEach(0 ..< temperatureUnitStrings.count) {
+                            Text("\(self.temperatureUnitStrings[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section(header: Text("Input in \(temperatureUnits[selectedInput])")) {
+                Section(header: Text("Input in \(temperatureUnitStrings[selectedInput])")) {
                     TextField("Enter value", text: $inputValue)
                         .keyboardType(.decimalPad)
                 }
                 Section(header: Text("Select output unit")) {
                     Picker("Input Unit", selection: $selectedOutput) {
-                        ForEach(0 ..< temperatureUnits.count) {
-                            Text("\(self.temperatureUnits[$0])")
+                        ForEach(0 ..< temperatureUnitStrings.count) {
+                            Text("\(self.temperatureUnitStrings[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section(header: Text("Output in \(temperatureUnits[selectedOutput])")) {
-                    Text("\(output, specifier: "%.2f")")
+                Section(header: Text("Output in \(temperatureUnitStrings[selectedOutput])")) {
+                    Text("\(output.value, specifier: "%.2f")")
                 }
             }
         .navigationBarTitle("Unit Converter")
@@ -88,29 +63,6 @@ struct ContentView: View {
     }
     
     // MARK: Methods
-    func convertCelsiusToFahr (_ input: Double) -> Double {
-        return (input * (9/5)) + 32
-    }
-    
-    func convertCelsiusToKelvin(_ input: Double) -> Double {
-        return input + 273.15
-    }
-    
-    func convertKelvinToCelsius(_ input: Double) -> Double {
-        return input - 273.15
-    }
-    
-    func convertKelvinToFahr(_ input: Double) -> Double {
-        return convertCelsiusToFahr(input - 273.15)
-    }
-    
-    func convertFahrToCelsius (_ input: Double) -> Double {
-        return (input - 32 ) * (9/5)
-    }
-    
-    func convertFahrToKelvin (_ input: Double) -> Double {
-        return convertFahrToCelsius(input) + 273.15
-    }
     
 }
 
