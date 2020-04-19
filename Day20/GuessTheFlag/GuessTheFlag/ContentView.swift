0a0a12b027131ad66f2ffc91be0dd1101c4e0e79
rolisanchez <roli.s91@gmail.com>
2020-04-19 18:20:26 +0800

    Day20

---
+++ Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Victor Rolando Sanchez Jara on 4/19/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+
+    var body: some View {
+        Text("Starting")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
