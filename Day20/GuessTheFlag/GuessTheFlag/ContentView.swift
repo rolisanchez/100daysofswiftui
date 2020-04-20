252ca2f25111a3f2da937d291ba1dc16f61d8a74
rolisanchez <roli.s91@gmail.com>
2020-04-20 16:04:11 +0800

    Day 22

--- Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
 
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    
+    @State private var score = 0
 
     // MARK: Body
     var body: some View {
@@ -44,11 +46,13 @@ struct ContentView: View {
 
                     }
                 }
+                Text("Your Score: \(score)")
+                .foregroundColor(.white)
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
         }
@@ -58,8 +62,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            score -= 1
         }
         
         showingScore = true
