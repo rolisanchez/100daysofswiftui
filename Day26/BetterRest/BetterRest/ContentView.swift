52fa566ab8990d1a6dbd4c60deab0b60b1cda9f5
rolisanchez <roli.s91@gmail.com>
2020-04-24 15:31:09 +0800

    Day27

--- Day26/BetterRest/BetterRest/ContentView.swift
+++ Day26/BetterRest/BetterRest/ContentView.swift
@@ -10,28 +10,92 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: Properties
+    
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date()
-
+    @State private var coffeeAmount = 1
+    
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     // MARK: Body
     var body: some View {
-        
-        return Form {
-            Section(header: Text("How much did you want to sleep?")) {
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+        NavigationView {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                }
+               
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(value: $coffeeAmount, in: 1...20) {
+                        Text("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s")")
+                    }
                 }
             }
-            Section(header: Text("When did you wake up?")) {
-                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents:  .hourAndMinute)
-                    .labelsHidden()
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
-            
         }
-        
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
     }
     
     // MARK: Methods
+    func calculateBedtime() {
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            alertMessage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is…"
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
