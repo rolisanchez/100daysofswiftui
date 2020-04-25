7ee168cb55c9ee07ec047ab452e57430aed9777d
rolisanchez <roli.s91@gmail.com>
2020-04-25 12:35:11 +0800

    Day29

---
+++ Day29/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Victor Rolando Sanchez Jara on 4/25/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Starting")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
