d481e3646eb7949433a4fb0ebd1716b6c6699787
rolisanchez <roli.s91@gmail.com>
2020-05-07 12:42:06 +0800

    Day41

---
+++ Day39/Mooonshot/Mooonshot/AstronautView.swift
@@ -0,0 +1,40 @@
+//
+//  AstronautView.swift
+//  Mooonshot
+//
+//  Created by Victor Rolando Sanchez Jara on 5/7/20.
+//  Copyright © 2020 Victor Rolando Sanchez Jara. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                    // Below not needed anymore. This bug was fixed
+//                    .layoutPriority(1)
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
